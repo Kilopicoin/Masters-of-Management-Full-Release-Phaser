bd2f154b90a2cc579888f6342c703997b99e1ea3
Cem Tosun <cemtosun@mail.com>
2024-10-05 00:19:02 +0300

    05 October 2024

---
+++ src/contract.js
@@ -0,0 +1,24 @@
+// contract.js
+import { BrowserProvider, Contract, JsonRpcProvider } from 'ethers';
+import contractABI from './contractABI.json'; // Import the ABI JSON file
+
+const contractAddress = '0x8ba10E8B2846853CD1d3C388A9b852Ac4d63A143';
+const RPC = 'https://api.s0.b.hmny.io';
+
+const getContract = async () => {
+  const provider = new JsonRpcProvider(RPC);
+  return new Contract(contractAddress, contractABI.abi, provider);
+};
+
+export const getSignerContract = async () => {
+  if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const provider = new BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    return new Contract(contractAddress, contractABI.abi, signer);
+  } else {
+    throw new Error('Ethereum wallet is not installed');
+  }
+};
+
+export default getContract;
